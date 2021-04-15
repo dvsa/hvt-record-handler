f91ff744450813bc6179e427f33b649786d410df
Anna Dodson <Anna.Dodson@DVSA.gov.uk>
2021-04-15 14:06:11 +0100

    Remove the .env file from prod builds
    - Move the copying of the .env file to the development
    build process so it's not included in the prod build

--- webpack.development.js
+++ webpack.development.js
@@ -1,7 +1,17 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const CopyPlugin = require('copy-webpack-plugin');
+
+const LAMBDA_NAME = 'RecordHandlerFunction'; // must correspond to lambda name in template.yml
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'source-map',
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: './.env', to: `.aws-sam/build/${LAMBDA_NAME}/` },
+      ],
+    }),
+  ]
 });
